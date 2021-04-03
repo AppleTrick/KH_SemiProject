96eb02cbb3eec6395a2fc6830df5cc3f79618a0e
B-joon <dosel45@gmail.com>
2021-04-03 17:32:58 +0900

    병합을 위한 커밋

--- Semi/src/com/controller/Crawling.java
+++ Semi/src/com/controller/Crawling.java
@@ -4,7 +4,9 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 
@@ -15,59 +17,168 @@ import org.jsoup.select.Elements;
 
 public class Crawling {
 	
-	public static void main(String[] args) throws IOException {
+	public List<String> CatCrawling(String savePath) throws IOException {
 		
-		String url = "https://www.daejeon.go.kr/ani/AniStrayAnimalList.do?flag=1&menuSeq=3108";
-		Document doc = null;
-		
+		List<String> successList = new ArrayList<String>();
+
+		// 크롤링할 웹 주소
+		String contentUrl = "https://www.daejeon.go.kr/ani/AniStrayAnimalList.do?flag=2&menuSeq=3090";
+		Document contentDoc = null;
+
 		try {
-			doc = Jsoup.connect(url).get();
+			// 웹과 연결
+			contentDoc = Jsoup.connect(contentUrl).get();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		Elements element = doc.select("ul.animal_list01");
-		
+		// 필요한 정보의 위치
+		Elements content = contentDoc.select("ul.animal_list01");
+
 		System.out.println("=============================================");
-		
-		Iterator<Element> ie1 = element.select("li > ul > li").iterator();
-		
+		// 지정한 위치를 저장
+		Iterator<Element> ie1 = content.select("li > ul > li").iterator();
+
+		List<String> contentList = new ArrayList<String>();
+		String val = "";
+		int count = 1;
+		// 5개 1세트를 만듬
 		while (ie1.hasNext()) {
-//			String ie = ie1.next().text();
-//			
-//			int index = ie.indexOf(":");
-//			
-//			String ie2 = ie.substring(0, index);
-//			String ie3 = ie.substring(index+1);
-			
-//			System.out.println("ie2 :" + ie2);
-//			System.out.println("ie3 :" + ie3);
-			System.out.println(ie1.next().text()+"\t");
+			val += ie1.next().text() + "\n";
+			if (count % 5 == 0) {
+				contentList.add(val);
+				val = "";
+			}
+			count++;
 		}
 		System.out.println("=============================================");
-//		
-//		Document doc = Jsoup.connect("https://www.daejeon.go.kr/ani/AniStrayAnimalList.do?flag=1&menuSeq=3108").get();
-//		
-//		String crawlingImg = "https://www.daejeon.go.kr";
-//		
-//		Element element = doc.select("ul.animal_list01").get(0);
-//		System.out.println(element);
-//		Elements img = element.select("img");
-//		System.out.println(img);
-//		int page = 0;
-//		for (Element e : img) {
-//			String url = crawlingImg+e.getElementsByAttribute("src").attr("src");
-//			System.out.println(url);
-//			URL imgUrl = new URL(url);
-//			System.out.println(imgUrl);
-//			BufferedImage jpg = ImageIO.read(imgUrl);
-//			System.out.println(jpg);
-//			File file = new File("C:\\Users\\kas78\\Desktop\\크롤링사진"+"\\"+page+".jpg");
-//			System.out.println(file);
-//			ImageIO.write(jpg, "jpg", file);
-//			if (url.contains("FileUpload")) {
-//				page+=1;
-//			}
-//		}
+		// 출력
+		for (int i = 0; i < contentList.size(); i++) {
+			System.out.println(contentList.get(i));
+			System.out.println("=============================================");
+		}
+		// 이미지 크롤링 하기위해 해당 웹과 연결
+		Document doc = Jsoup.connect("https://www.daejeon.go.kr/ani/AniStrayAnimalList.do?flag=2&menuSeq=3090").get();
+		// 이미지경로
+		String crawlingImg = "https://www.daejeon.go.kr";
+		// 해당 이미지가 담겨져 있는 상위 태그 클레스
+		Element element = doc.select("ul.animal_list01").get(0);
+		// 이미지가 있는 태그
+		Elements img = element.select("img");
+
+		for (Element e : img) {
+			// 크롤링할 이미지 경로
+			String url = crawlingImg+e.getElementsByAttribute("src").attr("src");
+			URL imgUrl = new URL(url);
+			BufferedImage jpg = ImageIO.read(imgUrl);
+			// db에 저장된 값과 중복되지 않게 특정 문자 치환
+			String name = url.replaceAll("/", "-").replaceAll("https:", "%");
+			System.out.println(name);
+			// 파일 저장 경로
+			File file = new File(savePath+"cat\\"+name);
+			ImageIO.write(jpg, "jpg", file);
+		}
+
+		String finishPath = savePath+"cat\\";
+		File finishDir = new File(finishPath);
+		File[] finishFileList = finishDir.listFiles();
+
+		// 값을 리턴해주기위해 successList에 저장
+		for (int i = 0; i < finishFileList.length; i++) {
+			for (File finishFile : finishFileList) {
+				if (finishFile.isFile()) {
+					String fileName = finishFile.getName();
+					String all = fileName + "@" + contentList.get(i);
+					successList.add(all);
+				}
+			}
+		}
+		
+		for (int i = 0; i < successList.size(); i++) {
+			System.out.println(successList.get(i));
+			System.out.println();
+		}
+		return successList;
+	}
+	public List<String> DokCrawling(String savePath) throws IOException {
+
+		List<String> successList = new ArrayList<String>();
+
+		// 크롤링할 웹 주소
+		String contentUrl = "https://www.daejeon.go.kr/ani/AniStrayAnimalList.do?flag=1&menuSeq=3090";
+		Document contentDoc = null;
+
+		try {
+			// 웹과 연결
+			contentDoc = Jsoup.connect(contentUrl).get();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		// 필요한 정보의 위치
+		Elements content = contentDoc.select("ul.animal_list01");
+
+		System.out.println("=============================================");
+		// 지정한 위치를 저장
+		Iterator<Element> ie1 = content.select("li > ul > li").iterator();
+
+		List<String> contentList = new ArrayList<String>();
+		String val = "";
+		int count = 1;
+		// 5개 1세트를 만듬
+		while (ie1.hasNext()) {
+			val += ie1.next().text() + "\n";
+			if (count % 5 == 0) {
+				contentList.add(val);
+				val = "";
+			}
+			count++;
+		}
+		System.out.println("=============================================");
+		// 출력
+		for (int i = 0; i < contentList.size(); i++) {
+			System.out.println(contentList.get(i));
+			System.out.println("=============================================");
+		}
+		// 이미지 크롤링 하기위해 해당 웹과 연결
+		Document doc = Jsoup.connect("https://www.daejeon.go.kr/ani/AniStrayAnimalList.do?flag=1&menuSeq=3090").get();
+		// 이미지경로
+		String crawlingImg = "https://www.daejeon.go.kr";
+		// 해당 이미지가 담겨져 있는 상위 태그 클레스
+		Element element = doc.select("ul.animal_list01").get(0);
+		// 이미지가 있는 태그
+		Elements img = element.select("img");
+
+		for (Element e : img) {
+			// 크롤링할 이미지 경로
+			String url = crawlingImg+e.getElementsByAttribute("src").attr("src");
+			URL imgUrl = new URL(url);
+			BufferedImage jpg = ImageIO.read(imgUrl);
+			// db에 저장된 값과 중복되지 않게 특정 문자 치환
+			String name = url.replaceAll("/", "-").replaceAll("https:", "%");
+			System.out.println(name);
+			// 파일 저장 경로
+			File file = new File(savePath+"dok\\"+name);
+			ImageIO.write(jpg, "jpg", file);
+		}
+
+		String finishPath = savePath+"dok\\";
+		File finishDir = new File(finishPath);
+		File[] finishFileList = finishDir.listFiles();
+
+		// 값을 리턴해주기위해 successList에 저장
+		for (int i = 0; i < finishFileList.length; i++) {
+			for (File finishFile : finishFileList) {
+				if (finishFile.isFile()) {
+					String fileName = finishFile.getName();
+					String all = fileName + "@" + contentList.get(i);
+					successList.add(all);
+				}
+			}
+		}
+		for (int i = 0; i < successList.size(); i++) {
+			System.out.println(successList.get(i));
+			System.out.println();
+		}
+		
+		return successList;
 	}
 }
