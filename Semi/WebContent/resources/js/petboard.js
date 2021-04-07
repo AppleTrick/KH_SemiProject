98f09300a0aaa1ad39a3b9d3a78d816e9e0b80ae
Changhee Park <arrans9591@gmail.com>
2021-04-08 00:41:41 +0900

    주석삭제

--- Semi/WebContent/resources/js/petboard.js
+++ Semi/WebContent/resources/js/petboard.js
@@ -73,50 +73,6 @@ function CloseModal(e) {
 	}
 }
 
-
-/*function start(){	
-	// 모달창 흑백화면
-	const modal = document.querySelector(".modal1");
-	// 이미지들
-	const previews = document.querySelectorAll(".photo-gallery img");
-	// 나오는 창
-	const original = document.querySelector(".full-img");
-	// 원래 이미지 파일
-	const thisImg = document.querySelector(".thisImg");
-
-	
-
-	previews.forEach(preview => {
-		preview.addEventListener('click', function(){
-			// modal 과 origianl 에 open class 를 추가한다.
-			modal.classList.add("open");
-			original.classList.add("open");
-			let ArticleNo = preview.getAttribute('articleNo');
-			console.log(ArticleNo);
-			document.getElementById('TextArticleNo').value=ArticleNo;
-			requestData(ArticleNo);
-			const originalSrc = preview.getAttribute("data-original");
-			if(preview.hasAttribute('chk')){
-				// 입양대기일 경우 입양신청 버튼 on / 이미지 경로 수정
-				document.getElementById('apply').style.display="inline";
-				thisImg.src = `resources/saveFile/${originalSrc}`;
-			} else{
-				// 입양후기일 경우 입양신청 버튼 off / 이미지 경로 수정
-				document.getElementById('apply').style.display="none";
-				thisImg.src = `savefile/${originalSrc}`;
-			}
-			// 오리지날 속성을 가지고 오고 클릭한 이미지의 를 오리지날 속성 이미지로 변경한다.
-		})
-	})
-	//흑백 화면 삭제시 class remove 작동
-	modal.addEventListener("click", (e) => {
-		if (e.target.classList.contains('modal1')) {
-			modal.classList.remove("open");
-			//original.classList.add("open");
-		}
-	})
-}*/
-
 // 누른 이미지 번호에 따른 data 요청
 async function requestData(ArticleNo) {
 	let data = await fetch("pet.do?command=titleContent&ArticleNo=" + ArticleNo).then(function(res) {
